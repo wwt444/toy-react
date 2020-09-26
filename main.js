ddfa5936b08d3c569852d09478a938c1921836ed
wwt444 <781248092@qq.com>
2020-09-26 23:18:43 +0800

    react-jsx原理实现

---
+++ main.js
@@ -0,0 +1,15 @@
+import { render, Component, creatElement } from './react-toy.js'
+
+class MyComponent extends Component {
+    render() {
+        return (<div>
+            <h1>my component</h1>
+            {this.children}
+        </div>)
+    }
+}
+render(<MyComponent id="a" class="c">
+    <div>333</div>
+    <div>1111</div>
+    <div>222</div>
+</MyComponent>,document.body) 
---
+++ react-toy.js
@@ -0,0 +1,61 @@
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type)
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value)
+    }
+    appendChild(component) {
+        this.root.appendChild(component.root)
+    }
+}
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content)
+    }
+
+}
+
+export class Component {
+    constructor() {
+        this.props = Object.create(null)
+        this.children = []
+        this._root = null
+    }
+    setAttribute(name, value) {
+        this.props[name] = value
+    }
+    appendChild(component) {
+        this.children.push(component)
+    }
+    get root() {
+        if (!this._root) {
+            this._root = this.render().root
+        }
+        return this._root
+    }
+}
+
+export function creatElement(type, attribute, ...chirldren) {
+    let e
+    (typeof type === "string") ? (e = new ElementWrapper(type)) : (e = new type)
+    for (let p in attribute) {
+        e.setAttribute(p, attribute[p])
+    }
+    let insertChildren = (chirldren) => {
+        for (let child of chirldren) {
+            if (typeof child === "string") {
+                child = new TextWrapper(child)
+            }
+            ((typeof child === "object") && (child instanceof Array)) ? (insertChildren(child) ): (e.appendChild(child))
+
+        }
+
+    }
+    insertChildren(chirldren)
+    return e
+}
+
+export function render(component, parentElement) {
+    parentElement.appendChild(component.root)
+}
